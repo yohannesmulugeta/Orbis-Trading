f0cd62a13186630ed913a81215b12115b726ef68
joni <70434578+yohannesmulugeta@users.noreply.github.com>
2026-08-18 19:04:41 +0300

    Use ORBIS repository imagery across core pages

--- lib/media.ts
+++ lib/media.ts
@@ -1,12 +1,14 @@
 export const webImages = {
-  mercedesSprinter:
-    "https://commons.wikimedia.org/wiki/Special:FilePath/2010_Mercedes-Benz_Sprinter_2500_Passenger_Van_%28W906%29.jpg",
-  renaultDuster: "https://commons.wikimedia.org/wiki/Special:FilePath/Renault_Duster_001.jpg",
-  mitsubishiFuso:
-    "https://commons.wikimedia.org/wiki/Special:FilePath/Mitsubishi_Fuso_Canter_%2827910526448%29.jpg",
+  mercedesSprinter: "/assets/mercedes-sprinter.jpg",
+  mercedesTruck: "/assets/mercedes-truck.jpg",
+  renaultDuster: "/assets/renault-duster.jpg",
+  renaultKangoo: "/assets/renault-kangoo.jpg",
+  mitsubishiFuso: "/assets/fuso-truck.jpg",
   ethiopianCoffee:
     "https://commons.wikimedia.org/wiki/Special:FilePath/Bunna4_-_Roasting_coffee_beans.jpg",
-  ethiopianBus: "https://commons.wikimedia.org/wiki/Special:FilePath/Selam_bus_Ethiopia.jpg",
-  automotiveWorkshop:
-    "https://commons.wikimedia.org/wiki/Special:FilePath/PC_kinyanjui_Automotive_Workshop.jpg",
+  ethiopianBus: "/assets/orbis-bus.jpg",
+  automotiveWorkshop: "/assets/bus-workshop.jpg",
+  orbisShowroom: "/assets/orbis-showroom.jpg",
+  orbisBuilding: "/assets/orbis-building.jpg",
+  ethiopianRoad: "/assets/ethiopian-road.jpg",
 };
